1f4368b14de5d8cdcba7c16844b1b8a97fefe8e2
Basil Victor <basilbennedict@gmail.com>
2020-11-10 17:38:04 +0530

    Return Books

--- dbms-library-project-angular/src/app/app-routing/routes.ts
+++ dbms-library-project-angular/src/app/app-routing/routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from '../login/login.component';
 import { HomeComponent } from '../home/home.component';
 import { BookDetailComponent } from '../book-detail/book-detail.component';
 import { FinesComponent } from '../fines/fines.component';
+import { ReturnComponent } from '../return/return.component';
 
 export const routes: Routes = [
   { path: 'login',  component: LoginComponent },
   { path: 'home',  component: HomeComponent },
   { path: 'book/:book_id',  component: BookDetailComponent },
   { path: 'fines', component: FinesComponent},
+  { path: 'return', component: ReturnComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
--- dbms-library-project-angular/src/app/return/return.component.ts
+++ dbms-library-project-angular/src/app/return/return.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  issue_id: number;
+  due_date: string;
+  book_title: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {issue_id: 1, due_date: '2020-10-10', book_title: "Yolo"}
+];
 
 @Component({
   selector: 'app-return',
@@ -7,9 +18,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReturnComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  displayedColumns: string[] = ['issue_id', 'book_title', 'due_date', 'return'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
